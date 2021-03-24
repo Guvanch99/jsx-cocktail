d6011a1b13afd7b40a0c9aa2f97a844e38283bcc
Guvanch Avediyev <awediyewguwanc@gmail.com>
2021-03-24 13:58:03 -0700

    Updates

---
+++ precache-manifest.4ffe5ead71c8bd60b7655e2a63f1aa5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8b7869d6d91bcc73c1e7b01bcaeb7c",
+    "url": "/jsx-cocktail/index.html"
+  },
+  {
+    "revision": "de0cf0c2ca03d5e0f6f4",
+    "url": "/jsx-cocktail/static/css/main.003e4849.chunk.css"
+  },
+  {
+    "revision": "555855e4790a85469098",
+    "url": "/jsx-cocktail/static/js/2.591603a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jsx-cocktail/static/js/2.591603a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0cf0c2ca03d5e0f6f4",
+    "url": "/jsx-cocktail/static/js/main.8f1b4b5a.chunk.js"
+  },
+  {
+    "revision": "98e37aa11e51b0f0dc69",
+    "url": "/jsx-cocktail/static/js/runtime-main.d6153e42.js"
+  }
+]);
